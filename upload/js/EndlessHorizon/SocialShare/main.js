544c792e960f765a8156a1bad62014f6ab75498b
Bobby Wibowo <bobbywibowo98@gmail.com>
2016-02-14 18:39:03 +0700

    Version 1.2.1 - Initial public release

---
+++ upload/js/EndlessHorizon/SocialShare/main.js
@@ -0,0 +1,106 @@
+var logPrefix = '[Endless Horizon] Social Share: ';
+
+function out(m){
+	return console.log(logPrefix + m);
+}
+
+function encodeURIComponentStrict(t){
+	t = encodeURIComponent(t);
+	t = t.replace('~' , '%7e'); // replace ~
+	t = t.replace('\'', '%27'); // replace '
+	return t;
+}
+
+$(document).ready(function(){
+	// initialize elements
+	var sharePageButtons = $('.button.primary.eh_socialshare');
+	var sharePagePopup = $('<div class="eh_socialshare_popup"></div>');
+	var shareCounts = $.parseJSON(ehss_settings["data-counts"]);
+	var enabledItemsCount = 0;
+	var enabledShareCounter = 0;
+	
+	// initialize popup
+	sharePagePopup.attr('style', 'display: none');
+	sharePagePopup.click(function(e){
+		//$('body').children().removeClass('blurred'); -- remove blur effect from the background
+		$(this).fadeToggle(400, "swing", function(){
+			$('#siropuChatBar').toggle();
+			$('#uix_jumpToFixed').removeClass('alwaysHidden');
+		});
+	});
+	
+	$('body').append(sharePagePopup);
+	
+	// initialize popup items
+	var shareItem, replacedURL, i, j, x;
+	
+	sharePagePopup.append($('<div class="items_container"><div class="centered"><ul></ul></div></div>'));
+	
+	for (i = 0; i < ehss_social_sites.length; i++) {
+		if (!ehss_social_sites[i].disabled) {
+			replacedURL = ehss_social_sites[i].popupURL;
+			for (j = 0; j < ehss_replace_methods.length; j++) {
+				if (typeof ehss_replace_methods[j].value === 'string') {
+					x = encodeURIComponentStrict(ehss_settings[ehss_replace_methods[j].value]);
+				} else {
+					switch (ehss_replace_methods[j].value) {
+						case 1  : x = encodeURIComponentStrict(document.title); // 1 - document.title
+							break;
+						case 2  : x = encodeURIComponentStrict($('link[rel="apple-touch-icon"]').attr('href')); // 2 - get image from apple-touch-icon
+							break;
+						// extend if necessary
+						default : x = '';
+					}
+				}
+				
+				replacedURL = replacedURL.replace(ehss_replace_methods[j].key, x);
+			}
+			
+			shareItem = $('<li><a></a></li>');
+			shareItem.find('a').attr('href', replacedURL);
+			shareItem.find('a').attr('data-name', ehss_social_sites[i].popupName);
+			shareItem.find('a').attr('data-specs', ehss_social_sites[i].popupSpecs);
+			
+			shareItem.find('a').append($('<span style="background-color: ' + ehss_social_sites[i].bgColor + '"></span>'));
+			shareItem.find('a').find('span').append($('<i class="' + ehss_social_sites[i].iconClass + '"></i>'));
+			
+			if ((typeof ehss_social_sites[i].shareCountID === 'string') && (ehss_social_sites[i].shareCountID.length > 0)) {
+				x = shareCounts[ehss_social_sites[i].shareCountID];
+				if (x !== -1) {
+					shareItem.find('a').find('span').append($('<i class="shareCount" style="color: ' + ehss_social_sites[i].bgColor + '" title="' + ehss_settings['data-count-title'].replace('{x}', x) +'">' + x + '</i>'));
+					enabledShareCounter += 1;
+				}
+			}
+			
+			shareItem.append($('<span>' + ehss_social_sites[i].name + '</span>'));
+			
+			sharePagePopup.find('.items_container .centered ul').append(shareItem);
+			
+			enabledItemsCount += 1;
+		}
+	}
+	
+	// prevent items from trigerring its parent's event handlers
+	sharePagePopup.find('.items_container .centered ul li').click(function(e){ e.stopPropagation(); });
+	
+	// register click event handler to all items
+	sharePagePopup.find('.items_container .centered ul li a').click(function(e){
+		e.preventDefault();
+		window.open($(this).attr('href'), $(this).attr('data-name'), $(this).attr('data-specs'));
+	});
+	
+	sharePageButtons.click(function(e){
+		e.preventDefault();
+		if (sharePagePopup.css('display') == 'none') {
+			//$('body').children().not('.arthref,script,.eh_socialshare_popup').addClass('blurred'); -- this will blur the background on browsers that supports CSS3 filter (disabled by default to match XenForo's overlay effect)
+			$('#siropuChatBar').toggle();
+			$('#uix_jumpToFixed').addClass('alwaysHidden');
+			sharePagePopup.fadeToggle(400, "swing");
+		}
+	});
+	
+	// hide buttons if no items were enabled
+	
+	out(enabledItemsCount + ' item(s) for the social share widget were initialized.');
+	out('Share counter was initialized for ' + enabledShareCounter + ' item(s).');
+});
---
+++ upload/library/EndlessHorizon/SocialShare/Listener.php
@@ -0,0 +1,134 @@
+<?php
+
+class EndlessHorizon_SocialShare_Listener
+{
+	private static function getCount($service, $url) {
+		$shareLinks = array(
+			"facebook"    => "https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27{url}%27",
+            "twitter"     => "http://opensharecount.com/count.json?url={url}",
+            "googleplus"  => "https://plusone.google.com/u/0/_/+1/fastbutton?count=true&url={url}",
+            "linkedin"    => "https://www.linkedin.com/countserv/count/share?format=json&url={url}",
+            "pinterest"   => "https://api.pinterest.com/v1/urls/count.json?url={url}",
+            "delicious"   => "https://feeds.delicious.com/v2/json/urlinfo/data?url={url}",
+            "buffer"      => "https://api.bufferapp.com/1/links/shares.json?url={url}",
+			"vk"          => "https://vk.com/share.php?act=count&index=1&url="
+        );
+		
+		$shareUrl = str_replace("{url}", $url, $shareLinks[$service]);
+		
+		if (function_exists('curl_version')) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $shareUrl);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT_MS, 500);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            $result = curl_exec($ch);
+            curl_close($ch);
+        } else {
+            $result = @file_get_contents($shareUrl);
+        }
+		
+		if ($result) {
+            switch($service) {
+				case "facebook":
+					$result = json_decode($result);
+					$count = $result->data[0]->total_count;
+					break;
+				case "google":
+					preg_match( '/window\.__SSR = {c: (\d+(?:\.\d+)+)/', $result, $matches);
+					if(isset($matches[0]) && isset($matches[1])) {
+						$bits = explode('.',$matches[1]);
+						$count = (int)( empty($bits[0]) ?: $bits[0]) . ( empty($bits[1]) ?: $bits[1] ); 
+					}
+					break;
+				case "pinterest":
+					$result = substr($result, 13, -1);
+				case "linkedin":
+				case "twitter":
+					$result = json_decode($result);
+					$count = $result->count;
+					break;
+				case "delicious":
+					$result = json_decode($result);
+					$count = $result[0]->total_posts;
+					break;
+				case "buffer":
+					$result = json_decode($result);
+					$count = $result->shares;
+					break;
+				case "vk":
+					$result = preg_match('/^VK.Share.count\(\d+,\s+(\d+)\);$/i', $result, $matches);
+					$count = $matches[1];
+					break;
+				default: break;
+            }
+			$count = (int) $count;
+        } else {
+           $count = -1;
+        }
+		
+		return $count;
+	}
+	
+	private static function cacheHitDebug($message) {
+		$permitted = XenForo_Application::get('options')->EHSS_CacheHitDebug;
+		
+		if (!$permitted) { return false; }
+		
+		return XenForo_Error::logException(new XenForo_Exception($message));
+	}
+	
+	public static function getShareCounts() {
+		$siteList = array("facebook", "twitter", "googleplus", "linkedin", "pinterest", "delicious", "buffer", "vk");
+		$counts = array();
+		
+		$url = XenForo_Application::get('requestPaths')['fullUri'];
+		
+		$cacheId = "ehss_".sprintf('%u', crc32($url)); // crc32 hash with 'ehss_' prefix
+		$cacheObject = XenForo_Application::getCache();
+		$cacheTime = XenForo_Application::get('options')->EHSS_CacheTime;
+		$previousCache = false;
+		
+		if ($cacheObject) {
+			$previousCache = $cacheObject->load($cacheId);
+		
+			if ($previousCache) {
+				$counts = json_decode($previousCache);
+				
+				foreach ($counts as $value) {
+					if ($value !== -1) {
+						self::cacheHitDebug('Share counters were loaded from cache and the data was valid - Cache ID: '.$cacheId);
+						return json_encode($counts); // Immediately return counts if at least one count was valid
+					}
+				}
+			}
+		}
+		
+		// Fetch share counts if there was no previous cache or the previous cache held no valid counts
+		$keepTrying = XenForo_Application::get('options')->EHSS_KeepTrying;
+		$completeFailure = true;
+		
+		foreach ($siteList as $site) {
+			$counts[$site] = -1;
+			
+			if (XenForo_Application::get('options')->EHSS_ShareCounter[$site]) { $counts[$site] = self::getCount($site, $url); }
+			
+			if ($counts[$site] !== -1) { $completeFailure = false; }
+		}
+			
+		if ($completeFailure && $keepTrying) {
+			self::cacheHitDebug('WARNING: Did not save share counters because none were successfully fetched - Cache ID: '.$cacheId);
+		} else {
+			if ($cacheObject) {
+				$cacheObject->save(json_encode($counts), $cacheId, array(), $cacheTime);
+				self::cacheHitDebug('Share counters were stored on cache - Cache ID: '.$cacheId);
+			} else {
+				self::cacheHitDebug('WARNING: Did not save share counters because of missing cache object - Cache ID: '.$cacheId);
+			}
+		}
+		
+		return json_encode($counts);
+	}
+}
+
+?>
