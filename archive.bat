6241de0b23cfe60f371a1739c947f64d6cc78129
Bobby Wibowo <bobbywibowo98@gmail.com>
2016-02-15 20:26:52 +0700

    Added my personal archiver batch file (relies on 7-Zip)

---
+++ archive.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Package files excluding .git folder with 7-Zip command line (assuming 7-Zip is installed on C:\Program Files\7-Zip)
+
+"C:\Program Files\7-Zip\7z.exe" a -x!.git addon-EndlessHorizonSocialShare.zip
+echo.
+pause
