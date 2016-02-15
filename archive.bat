b54a7f17625f2de65c23cf7204782b4a298d1dfa
Bobby Wibowo <bobbywibowo98@gmail.com>
2016-02-15 20:29:03 +0700

    Update archive.bat

--- archive.bat
+++ archive.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Package files excluding .git folder with 7-Zip command line (assuming 7-Zip is installed on C:\Program Files\7-Zip)
+REM Package files excluding git-related folders and files with 7-Zip command line (assuming 7-Zip is installed on C:\Program Files\7-Zip)
 
 "C:\Program Files\7-Zip\7z.exe" a -x!.git -x!archive.bat -x!.gitignore addon-EndlessHorizonSocialShare.zip
 echo.
