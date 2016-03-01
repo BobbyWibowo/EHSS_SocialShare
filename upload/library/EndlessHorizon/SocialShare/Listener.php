d2e600a1c0957222ecb0617a7bfc2cb1c83155e5
Bobby Wibowo <bobbywibowo98@gmail.com>
2016-03-01 13:31:47 +0700

    Added alt. method for Facebook, etc.

--- upload/library/EndlessHorizon/SocialShare/Listener.php
+++ upload/library/EndlessHorizon/SocialShare/Listener.php
@@ -42,9 +42,10 @@ class EndlessHorizon_SocialShare_Listener
         }
     }
     
-    private static function getCount($service, $siteUrl, $timeout, $useCurl, $useMS, $useSsl, $useSslVerifyPeer) {
+    private static function getCount($service, $siteUrl, $curlTimeout, $useCurl, $curlVerPassMS, $curlSslVerify, $curlVerPassPeer, $curlCertInfo, $extraInfo) {
         $shareLinks = array(
             "facebook"    => "https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27{url}%27",
+            "facebook_v2" => "https://graph.facebook.com/?id={url}",
             "twitter"     => "http://opensharecount.com/count.json?url={url}",
             "googleplus"  => "https://plusone.google.com/u/0/_/+1/fastbutton?count=true&url={url}",
             "linkedin"    => "https://www.linkedin.com/countserv/count/share?format=json&url={url}",
@@ -54,6 +55,11 @@ class EndlessHorizon_SocialShare_Listener
         );
         
         $url = str_replace("{url}", rawurlencode($siteUrl), $shareLinks[$service]);
+
+        // Facebook v2 patch (use newer Graph API with access token when available)
+        if ($service === 'facebook_v2') {
+            $url = $url.'&access_token='.$extraInfo['facebookAppId'].'|'.$extraInfo['facebookAppSecret'];
+        }
         
         if ($useCurl) {
             $ch = curl_init();
@@ -61,21 +67,27 @@ class EndlessHorizon_SocialShare_Listener
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-            if ($useMS) {
-                curl_setopt($ch, CURLOPT_TIMEOUT_MS, $timeout);
+            if ($curlVerPassMS) {
+                curl_setopt($ch, CURLOPT_TIMEOUT_MS, $curlTimeout);
             } else {
-                $tmp = ($timeout/1000);
+                $tmp = ($curlTimeout/1000);
                 curl_setopt($ch, CURLOPT_TIMEOUT, (int)ceil($tmp));
             }
 
-            if ($useSsl) {
-                curl_setopt($ch, CURLOPT_CAINFO, getcwd().'/library/EndlessHorizon/SocialShare/cacert.pem'); // Taken from https://curl.haxx.se/ca/cacert.pem
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-                if ($useSslVerifyPeer) { curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1); }
+            if ($curlSslVerify) {
+                if ($curlCertInfo) {
+                    curl_setopt($ch, CURLOPT_CAINFO, getcwd().'/library/EndlessHorizon/SocialShare/cacert.pem'); // Taken from https://curl.haxx.se/ca/cacert.pem
+                }
+                if ($curlVerPassPeer) {
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+                }
             } else {
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-                if ($useSslVerifyPeer) { curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); }
+                if ($curlVerPassPeer) {
+                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                }
             }
+
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, ($curlSslVerify ? 2 : 0));
             curl_setopt($ch, CURLOPT_FAILONERROR, 1);
 
             $result = curl_exec($ch);
@@ -93,12 +105,16 @@ class EndlessHorizon_SocialShare_Listener
             switch($service) {
                 case "facebook":
                     $result = json_decode($result);
-                    $count = $result->data[0]->total_count;
+                    $count  = $result->data[0]->total_count;
+                    break;
+                case "facebook_v2":
+                    $result = json_decode($result);
+                    $count  = $result->share->share_count;
                     break;
                 case "googleplus":
                     preg_match( '/window\.__SSR = {c: (\d+(?:\.\d+)+)/', $result, $matches);
                     if(isset($matches[0]) && isset($matches[1])) {
-                        $bits = explode('.',$matches[1]);
+                        $bits  = explode('.',$matches[1]);
                         $count = (int)(empty($bits[0]) ?: $bits[0]); 
                     }
                     break;
@@ -107,15 +123,15 @@ class EndlessHorizon_SocialShare_Listener
                 case "linkedin":
                 case "twitter":
                     $result = json_decode($result);
-                    $count = $result->count;
+                    $count  = $result->count;
                     break;
                 case "buffer":
                     $result = json_decode($result);
-                    $count = $result->shares;
+                    $count  = $result->shares;
                     break;
                 case "vk":
                     $result = preg_match('/^VK.Share.count\(\d+,\s+(\d+)\);$/i', $result, $matches);
-                    $count = $matches[1];
+                    $count  = $matches[1];
                     break;
                 default: break;
             }
@@ -153,31 +169,48 @@ class EndlessHorizon_SocialShare_Listener
             }
         }
         
-        // Fetch share counts if there was no previous cache or the previous cache held no valid counts
+        
+        // Load XenForo options
         $keepTrying      = XenForo_Application::get('options')->EHSS_KeepTrying;
         $completeFailure = true; // Mark current session as completely failed
-        
+
         $curlTimeout     = XenForo_Application::get('options')->EHSS_CurlTimeout;
         $curlDisabled    = XenForo_Application::get('options')->EHSS_DisableCurl;
         $curlExist       = function_exists('curl_version');
         $curlVerPassMS   = false;
         $curlSslVerify   = XenForo_Application::get('options')->EHSS_CurlSslVerify;
         $curlVerPassPeer = false;
-        
+        $curlCertInfo    = XenForo_Application::get('options')->EHSS_CurlCertInfo;
+
+        // Fetch share counts if there was no previous cache or the previous cache held no valid counts
         if ($curlExist) {
             $curlVerPassMS   = self::doesComplyWithMinCurlVersion('7.16.2');
             $curlVerPassPeer = self::doesComplyWithMinCurlVersion('7.10');
 
             $tmp = curl_version();
             if (!$curlVerPassMS) { self::logExceptionByType('DEBUG: Did not use milliseconds as cURL timeout because cURL version was older than 7.16.2 (version: '.$tmp['version'].')', 2); }
-            if (!$curlVerPassPeer) { self::logExceptionByType('DEBUG: Did not cURL option \'CURLOPT_SSL_VERIFYPEER\' because cURL version was older than 7.10 (version: '.$tmp['version'].')', 2); }
+            if (!$curlVerPassPeer) { self::logExceptionByType('DEBUG: Did not use cURL option \'CURLOPT_SSL_VERIFYPEER\' because cURL version was older than 7.10 (version: '.$tmp['version'].')', 2); }
         }
         
         foreach ($services as $service) {
+            $extraInfo = array();
+            $tmpService = $service;
             $counts[$service] = -1;
+
+            // Facebook v2 patch (use newer Graph API with access token when available)
+            if ($tmpService === 'facebook') {
+                $facebookAppId     = XenForo_Application::get('options')->facebookAppId;
+                $facebookAppSecret = XenForo_Application::get('options')->facebookAppSecret;
+                if (strlen($facebookAppId) && strlen($facebookAppSecret)) {
+                    self::logExceptionByType('DEBUG: Facebook App ID and Secret were available on XenForo options, thus the add-on would use an alternative of the Facebook API', 2);
+                    $tmpService = 'facebook_v2';
+                    $extraInfo['facebookAppId']     = $facebookAppId;
+                    $extraInfo['facebookAppSecret'] = $facebookAppSecret;
+                }
+            }
             
             $tmp = XenForo_Application::get('options')->EHSS_ShareCounter;
-            if ($tmp[$service]) { $counts[$service] = self::getCount($service, $siteUrl, $curlTimeout, (!$curlDisabled && $curlExist ? true : false), $curlVerPassMS, $curlSslVerify, $curlVerPassPeer); }
+            if ($tmp[$service]) { $counts[$service] = self::getCount($tmpService, $siteUrl, $curlTimeout, (!$curlDisabled && $curlExist ? true : false), $curlVerPassMS, $curlSslVerify, $curlVerPassPeer, $curlCertInfo, $extraInfo); }
             
             if ($counts[$service] !== -1) {
                 $completeFailure = false; // If at least one service was fetched properly, mark current session as not completely failed, then store to cache
